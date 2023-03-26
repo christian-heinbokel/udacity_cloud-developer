a3821164499dcf1ec3ec6c05ff01146e29eb6d8f
Lennart Heinbokel <lennart@heinbokel.net>
2023-03-26 15:55:46 +0200

    removes commented out domain in config.ts

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -4,7 +4,6 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // DONE: Create an Auth0 application and copy values from it into this map. For example:
-  // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-8hc6rxazuwjucxld.us.auth0.com', // Auth0 domain
   clientId: 'rt70UlTpAngK1zFukvnjLDzlXlMAyD5L', // Auth0 client id
   callbackUrl: 'http://localhost:3001/callback'
