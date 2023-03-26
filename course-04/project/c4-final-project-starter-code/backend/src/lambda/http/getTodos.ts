1eed6eabc994e7a8dfdaf24952a2c7c862a84df7
Lennart Heinbokel <lennart@heinbokel.net>
2023-03-26 01:44:16 +0100

    remove manual cors headers from getTodos lambda

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -13,19 +13,13 @@ const logger = createLogger('getTodos')
 // DONE: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    logger.info('Processing event: ', event)
+    logger.info('ℹ️ GetTodos handler processes event: ', event)
 
     const userId = getUserId(event)
     const todos = await getTodosForUser(userId)
 
     return {
       statusCode: 200,
-      // CORS headers
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Credentials': true
-      },
-
       body: JSON.stringify({
         items: todos
       })
